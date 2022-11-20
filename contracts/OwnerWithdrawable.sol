8501856e4792a05f55242d0fe4bf4a5956547544
goreduxnow <116731472+goreduxnow@users.noreply.github.com>
2022-11-20 21:46:25 +0530

    Update OwnerWithdrawable.sol

--- contracts/OwnerWithdrawable.sol
+++ contracts/OwnerWithdrawable.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -12,10 +12,13 @@ contract OwnerWithdrawable is Ownable {
 
     fallback() external payable {}
 
-    function withdrawCurrency(uint256 amt) public onlyOwner {
+    function withdraw(address token, uint256 amt) public onlyOwner {
+        IERC20(token).transfer(msg.sender, amt);
+    }
+    function withdrawCurrency(uint256 amt) external onlyOwner {
         payable(msg.sender).transfer(amt);
     }
-    function getCurrencyBalance()public view returns(uint256){
+    function getCurrencyBalance()external view returns(uint256){
         return (address(this).balance);
     }
 
