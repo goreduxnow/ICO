6d00e951adbe0083a60c4f8de8238ec6e3022264
goreduxnow <116731472+goreduxnow@users.noreply.github.com>
2022-11-23 17:31:00 +0530

    Update OwnerWithdrawable.sol

--- contracts/OwnerWithdrawable.sol
+++ contracts/OwnerWithdrawable.sol
@@ -1,21 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract OwnerWithdrawable is Ownable {
-    using SafeMath for uint256;
 
     receive() external payable {}
 
     fallback() external payable {}
 
-    function withdrawCurrency(uint256 amt) external onlyOwner {
-        payable(msg.sender).transfer(amt);
+    /// @notice Owner can call this function to withdraw BNB
+    /// @param _amt Amount that needs to be withdrawn
+    function withdrawCurrency(uint256 _amt) external onlyOwner {
+        payable(msg.sender).transfer(_amt);
     }
-    function getCurrencyBalance()external view returns(uint256){
+    /// @notice Get the BNB balance of the presale contract
+    /// @@return Amount of BNB in wei
+    function getCurrencyBalance() external view returns(uint256){
         return (address(this).balance);
     }
 
